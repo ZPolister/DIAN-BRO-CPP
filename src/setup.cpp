f739f59c55c462fe48e1c24bdb4bc406e8263e2a
Polister <yyla05@foxmail.com>
2024-10-06 17:44:08 +0800

    fix: 修复跨平台声明的bug

--- src/setup.cpp
+++ src/setup.cpp
@@ -9,10 +9,29 @@ GAME_STATE gameState = RUNNING;
 bool QUICK_CLEAR = false;
 
 
-KEY getKey()
-{
-char c = (char)getch();
-c = (c == K_ESC) ? (char)getch() : c;
-c = (c == K_BRAC_OPEN) ? (char)getch() : c;
-return c;
+char getch() {
+    char buf = 0;
+    termios old = {0};
+    if (tcgetattr(0, &old) < 0)
+        perror("tcsetattr()");
+    old.c_lflag &= ~ICANON;
+    old.c_lflag &= ~ECHO;
+    old.c_cc[VMIN] = 1;
+    old.c_cc[VTIME] = 0;
+    if (tcsetattr(0, TCSANOW, &old) < 0)
+        perror("tcsetattr ICANON");
+    if (read(0, &buf, 1) < 0)
+        perror("read()");
+    old.c_lflag |= ICANON;
+    old.c_lflag |= ECHO;
+    if (tcsetattr(0, TCSADRAIN, &old) < 0)
+        perror("tcsetattr ~ICANON");
+    return (buf);
+}
+
+KEY getKey() {
+    char c = (char) getch();
+    c = (c == K_ESC) ? (char) getch() : c;
+    c = (c == K_BRAC_OPEN) ? (char) getch() : c;
+    return c;
 }
